7554a1254b8bf662172badf46b77d1be01e9ecb8
xSantyGx <santigiraldobi@gmail.com>
2022-06-01 15:27:23 -0500

    Proyecto
    Se añadio proyecto final

---
+++ 03fundamentos/prueba 2/src/co/edu/unbosque/model/Jugadores.java
@@ -0,0 +1,22 @@
+package co.edu.unbosque.model;
+
+public class Jugadores {
+
+	private int dado;
+
+	public Jugadores(int dado) {
+		this.dado = dado;
+	}
+
+	public int getDado() {
+		return dado;
+	}
+
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+	
+	
+	
+	
+}
